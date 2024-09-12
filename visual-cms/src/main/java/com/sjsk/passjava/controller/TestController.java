115050d191a2366f397205d50246ba11b2e67805
zhouyang <yang.zhou@vcg.com>
2024-09-12 17:54:07 +0800

    引入swagger

--- visual-cms/src/main/java/com/sjsk/passjava/controller/TestController.java
+++ visual-cms/src/main/java/com/sjsk/passjava/controller/TestController.java
@@ -7,6 +7,8 @@ import com.sjsk.passjava.model.Student;
 import com.sjsk.passjava.model.Teacher;
 import com.sjsk.passjava.service.StudentService;
 import com.sjsk.passjava.service.TeacherService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.apache.http.HttpHost;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.client.RequestOptions;
@@ -23,7 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-
+@Api(tags = "测试接口")
 @RestController
 @RequestMapping("test")
 public class TestController {
@@ -34,6 +36,7 @@ public class TestController {
     @Autowired
     private TeacherService teacherService;
 
+    @ApiOperation(value = "测试接口",notes = "测试接口说明")
     @GetMapping("test")
     public Object test() {
         Student one = studentService.lambdaQuery().last("limit 1").one();
